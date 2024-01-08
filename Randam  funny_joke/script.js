9d53555c1ba1518ac4c4e246ac58af113e76c435
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-01-08 22:47:45 +0530

    Random funny_joks

---
+++ Randam  funny_joke/script.js
@@ -0,0 +1,19 @@
+const jokeE1 = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+    const res = await fetch('https://icanhazdadjoke.com',config)
+
+    const data = await res.json()
+
+    jokeE1.innerHTML = data.joke
+}
