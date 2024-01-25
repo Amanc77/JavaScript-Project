35adfd1de4a1de1d489d7f309214161b9a486c3d
Aman Kumar Chaurasiya <148977902+AmanCode7@users.noreply.github.com>
2024-01-26 01:18:33 +0530

    Delete Digital Clock directory

--- Digital Clock/script.js
+++
@@ -1,7 +0,0 @@
- 
-const time = document.getElementById('clock')
-
-setInterval(function(){
-    let date = new Date()
-    time.innerHTML = date.toLocaleTimeString()
-},1000)//by defalt this is one second
