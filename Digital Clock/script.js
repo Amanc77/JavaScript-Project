5018ea6532a2a188709e573efe5ba9082526216a
Aman Kumar Chaurasiya <148977902+AmanCode7@users.noreply.github.com>
2024-01-26 01:20:53 +0530

    Digital clock and background changer

---
+++ Digital Clock/script.js
@@ -0,0 +1,43 @@
+ 
+const time = document.getElementById('clock')
+
+setInterval(function(){
+    let date = new Date()
+    time.innerHTML = date.toLocaleTimeString(undefined, { hour12: true });
+
+},1000)//by defalt this is one second
+
+
+
+
+
+//console.log(Math.random())
+
+const randomColor = function(){
+    const hex = '0123456789ABCDEF'
+    let color = '#'
+ 
+
+    for (let i = 0 ; i<6; i++){
+        color = color + hex[Math.floor(Math.random()*16)]
+    }
+    console.log(color)
+    const body = document.querySelector('body')
+    body.style.backgroundColor = color;
+}
+//randomColor();
+
+let start = document.querySelector('.start')
+
+let stop = document.querySelector('.stop')
+
+let setIntervalID
+
+ start.addEventListener('click',function(e){
+
+    setIntervalID = setInterval(randomColor,1000)
+
+})
+stop.addEventListener('click',function(e){
+    clearInterval(setIntervalID)
+})
