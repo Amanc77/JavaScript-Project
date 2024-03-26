20932ef1638b12825684867d9d99b95ea55a218a
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-03-26 13:58:57 +0530

    added some new feture

--- 15 basic project/7. Digital Clock/script.js
+++ 15 basic project/7. Digital Clock/script.js
@@ -50,16 +50,7 @@ const toggle = document.querySelector('.toggle')
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-toggle.addEventListener('click', (e) => {
-    const html = document.querySelector('html')
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
-    } else {
-        html.classList.add('dark')
-        e.target.innerHTML = 'Light mode'
-    }
-})
+
 
 function setTime() {
     const time = new Date();
